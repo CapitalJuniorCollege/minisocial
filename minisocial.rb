6b4fb83bafafac0b6e4318d34712d99c526ab208
Yonga <jorgeggayon@gmail.com>
2017-08-31 19:50:51 -0500

    Add logged_in_menu functionality and sign_up

--- minisocial.rb
+++ minisocial.rb
@@ -8,7 +8,6 @@ class Post
 
 end
 
-
 class User
 
   PSW_REGEX = /\A.{8}\z/
@@ -61,7 +60,6 @@ end
 # puts "authenticate #{user2.nickname} with 12345678"
 # puts user2.authenticate "12345678"
 
-
 def menu
   system("clear")
   puts '#'*60
@@ -77,47 +75,66 @@ def menu
   puts "0. Salir"
 end
 
+def logged_in_menu
+  system("clear")
+  puts '#'*60
+  puts '#'*60
+  puts "MINISOCIAL :)".rjust(40, '#') + '#'*20
+  puts "WELCOME - #{@current_user.nickname.blank? ?
+                    @current_user.email :
+                    @current_user.nickname}"
+  puts "_"*60
+  puts ""
+  puts "MENU"
+  puts ""
+  puts "1. Publique algo mi perrito"
+  puts "2. Ver publicaciones"
+  puts "3. Cerrar sesion"
+  puts "0. Salir"
+end
+
 def sign_up
+  system "clear"
   puts "Dijite su correo electronico."
   email = gets.chomp
   puts "Dijite su password."
   password = gets.chomp
   puts "Confirme su password."
   password_confirmation = gets.chomp
+  puts "Dijite su nickname."
+  nickname = gets.chomp
+  if password != password_confirmation
+    system "clear"
+    puts "Password no concide con la confirmacion, intentelo de nuevo, perrito. Presione cualquier tecla para continuar..."
+    gets.chomp
+    sign_up
+    return
+  end
+  if @users[email]
+    system "clear"
+    puts "Ya se encuentra un usuario registrado con este email. Crea una cuenta nueva. Presione cualquier tecla para continuar..."
+    gets.chomp
+    sign_up
+    return
+  end
+  @users[email] = User.new(email: email, password: password, nickname: nickname)
+  @current_user = @user
 end
 
 @input = nil
+@users = {}
+@current_user = nil
 while @input != 0 do
-  menu
+  if @current_user
+    logged_in_menu
+  else
+    menu
+    case @input
+    when 1 then sign_up
+    end
+    puts @users.inspect
+    menu
+  end
   @input = gets.chomp.to_i
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #
