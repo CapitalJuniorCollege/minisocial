dfa0d07bcfe636c737ba885f9b0fead3daf16be2
Yonga <jorgeggayon@gmail.com>
2017-08-31 19:08:46 -0500

    Init repo with Post and User class

---
+++ minisocial.rb
@@ -0,0 +1,72 @@
+class Post
+
+  attr_accessor :body
+
+  def initialize(opts = {})
+    @body = opts[:body].to_s
+  end
+
+end
+
+
+class User
+
+  PSW_REGEX = /\A.{8}\z/
+  EMAIL_REGEX = /\A\p{Alnum}+@\p{Alnum}+\.com\z/
+  attr_accessor :email, :password, :nickname, :posts
+
+  def initialize(opts = {})
+    #@email = opts[:email] ? opts[:email] : raise(Exception.new("PailaMiPerro"))
+    @email = opts[:email].to_s
+              .scan(EMAIL_REGEX).any? ?
+              opts[:email] :
+              abort("PailaMiPerro eso no es un email")
+    @password = opts[:password].to_s.scan(PSW_REGEX).any? ? opts[:password] : abort("PailaMiPerro eso no es un password valido")
+    @nickname = opts[:nickname].to_s
+    @posts = opts[:posts].is_a?(Array) ? opts[:posts] : []
+  end
+
+  # crea un Post y lo agrega al arreglo de posts
+  # parameters: opts = {}. opts[:body], recibe el body del post en un hash.
+  def create_post(opts = {})
+    self.posts << Post.new(opts)
+  end
+
+  def authenticate(password_submitted)
+    self.password == password_submitted.to_s
+  end
+
+end
+
+user = User.new(
+  email: "yonga@fusepong.com",
+  password: "holaqueh",
+  nickname: "yonga"
+)
+user2 = User.new(
+  email: "camilo@fusepong.com",
+  password: "12345678",
+  nickname: "camilo"
+)
+
+user.create_post(body: "hola que hace, banderiando o que hace??")
+puts user.posts.inspect
+puts user.posts.first
+puts user.posts.first.class
+puts user.posts.first.inspect
+puts user.posts.first.body
+
+puts "authenticate #{user.nickname} with 12345678"
+puts user.authenticate "12345678"
+puts "authenticate #{user2.nickname} with 12345678"
+puts user2.authenticate "12345678"
+
+
+
+
+
+
+
+
+
+#
