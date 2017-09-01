b32ed429fb34b1be9cffd26f01db05d6fa15e8ae
Yonga <jorgeggayon@gmail.com>
2017-08-31 19:20:47 -0500

    Add menu and first option

--- minisocial.rb
+++ minisocial.rb
@@ -38,28 +38,79 @@ class User
 
 end
 
-user = User.new(
-  email: "yonga@fusepong.com",
-  password: "holaqueh",
-  nickname: "yonga"
-)
-user2 = User.new(
-  email: "camilo@fusepong.com",
-  password: "12345678",
-  nickname: "camilo"
-)
+# user = User.new(
+#   email: "yonga@fusepong.com",
+#   password: "holaqueh",
+#   nickname: "yonga"
+# )
+# user2 = User.new(
+#   email: "camilo@fusepong.com",
+#   password: "12345678",
+#   nickname: "camilo"
+# )
+#
+# user.create_post(body: "hola que hace, banderiando o que hace??")
+# puts user.posts.inspect
+# puts user.posts.first
+# puts user.posts.first.class
+# puts user.posts.first.inspect
+# puts user.posts.first.body
+#
+# puts "authenticate #{user.nickname} with 12345678"
+# puts user.authenticate "12345678"
+# puts "authenticate #{user2.nickname} with 12345678"
+# puts user2.authenticate "12345678"
 
-user.create_post(body: "hola que hace, banderiando o que hace??")
-puts user.posts.inspect
-puts user.posts.first
-puts user.posts.first.class
-puts user.posts.first.inspect
-puts user.posts.first.body
 
-puts "authenticate #{user.nickname} with 12345678"
-puts user.authenticate "12345678"
-puts "authenticate #{user2.nickname} with 12345678"
-puts user2.authenticate "12345678"
+def menu
+  system("clear")
+  puts '#'*60
+  puts '#'*60
+  puts "MINISOCIAL :)".rjust(40, '#') + '#'*20
+  puts "WELCOME".rjust(35, '#') + '#'*25
+  puts "_"*60
+  puts ""
+  puts "MENU"
+  puts ""
+  puts "1. Registrarse"
+  puts "2. Iniciar sesion"
+  puts "0. Salir"
+end
+
+def sign_up
+  puts "Dijite su correo electronico."
+  email = gets.chomp
+  puts "Dijite su password."
+  password = gets.chomp
+  puts "Confirme su password."
+  password_confirmation = gets.chomp
+end
+
+@input = nil
+while @input != 0 do
+  menu
+  @input = gets.chomp.to_i
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
